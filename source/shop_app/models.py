142faa738129d310d8d93db62beb9a8ecd9657f7
Isa Bakhtov <issab79@icloud.com>
2022-10-16 21:52:17 +0600

    Добавден бустрап для удобства и аккуратности страницю Изменение в файле models.py связан с предыдущим коммитом.

--- source/shop_app/models.py
+++ source/shop_app/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models import TextChoices
+
 
 # Create your models here.
+class CategoryChoices(TextChoices):
+    PHONES = 'Phones', 'Телефоны'
+    ACCUMULATORS = 'Accumulators', 'Аккумуляторы'
+    NOTEBOOKS = 'Notebooks', 'Ноутбуки'
+    OTHERS = 'Others', 'Другие'
+
+
 class Good(models.Model):
-    CATEGORIES_VAR = (
-        ('Ph', 'Phones'),
-        ('Acc', 'Accumulators'),
-        ('Nb', 'Notebooks'),
-        ('O', 'Others') 
-    )
     
     title = models.CharField(verbose_name='Title',max_length=100, null=False, blank=False)
     description = models.TextField(verbose_name='Description',max_length=2000, null=True, default="No description")
     photo = models.CharField(verbose_name="Image", max_length=500, null=True, blank=False, default="No photo")
-    category = models.CharField(verbose_name="Category", max_length=20, choices=CATEGORIES_VAR, default=CATEGORIES_VAR[3][1])
+    category = models.CharField(verbose_name="Category", max_length=20, choices=CategoryChoices.choices, default=CategoryChoices.OTHERS)
     balance = models.PositiveIntegerField(verbose_name='Balance',null=False, blank=False)
     price = models.DecimalField(verbose_name='Price', max_digits=7, decimal_places=2, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
@@ -23,7 +26,8 @@ class Good(models.Model):
     
 
     def __str__(self):
-        return f"{self.title} - {self.description} - {self.price}"
+        return f"{self.title} - {self.description} - {self.price}"        
+    
     
     def delete(self, using=None, keep_parents=False):
         self.deleted_at = timezone.now()
